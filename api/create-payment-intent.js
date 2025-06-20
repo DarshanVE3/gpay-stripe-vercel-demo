e8c1ce1a612535c3c1fe42c52a3a9a3cf72af686
darshankangad7878@gmail.com <Darshan.kangad@ve3.global>
2025-06-20 10:13:34 +0530

    first commit

---
+++ api/create-payment-intent.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  const { amount } = req.body;
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: 'usd',
+      automatic_payment_methods: { enabled: true },
+    });
+
+    res.status(200).json({ clientSecret: paymentIntent.client_secret });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
